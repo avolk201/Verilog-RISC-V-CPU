5f2ddba7121e5f9043dcc0817bdfabed05e25a95
Alexander Volkov <volkovalexander201@gmail.com>
2025-05-26 13:58:05 +1000

    added some formatting stuff to the assembly programs

--- Compiler/fibonacci.asm
+++ Compiler/fibonacci.asm
@@ -1,36 +1,31 @@
-// Functional opcodes: ADD, SUB, XOR, LDI (load data immediate), JMP (jump/goto), BEQZ (branch if equals zero), HALT, STR (store), AND, LOAD, MOV (move), JAL, JR, MUL
-// Registers:
-//   R0 = F[n-1]
-//   R1 = F[n]
-//   R2 = F[n+1] (temporary next value)
-//   R3 = 0       (constant zero)
-//   R4 = memory address pointer
-//   R5 = loop counter (how many more fibs to do)
-//   R6 = 1
+# Functional opcodes: ADD, SUB, XOR, LDI (load data immediate), JMP (jump/goto), BEQZ (branch if equals zero), HALT, STR (store), AND, LOAD, MOV (move), JAL, JR, MUL
+# Registers:
+#   R0 = F[n-1]
+#   R1 = F[n]
+#   R2 = F[n+1] (temporary next value)
+#   R3 = 0       (constant zero)
+#   R4 = memory address pointer
+#   R5 = loop counter (how many more fibs to do)
+#   R6 = 1
 
-    // Load initial values
-    LDI R0, #0      // F[0] = 0
-    LDI R1, #1      // F[1] = 1
-    LDI R2, #0      // temp
-    LDI R3, #0      // zero constant
-    LDI R4, #1      // R4 = memory address pointer
-    LDI R5, #24     // number of loops to do
-    LDI R6, #1      // decrement amount
+# Load initial values
+LDI R0, 0      # F[0] = 0
+LDI R1, 1      # F[1] = 1
+
+LDI R2, 0      # temp
+LDI R3, 0      # zero constant
+LDI R4, 1      # R4 = memory address pointer
+LDI R5, 25     # number of loops to do
+LDI R6, 1      # decrement amount
+ADD R9, R9, R6
 
 LOOP:
-    // store current F[n] (R1) to memory at address R4
-    STR  R0, R4
-    // compute next = R0 + R1
-    ADD  R2, R0, R1
 
-    // increment address pointer
-    ADD  R4, R4, R6
-
-    // shift window
-    MOV  R0, R1  // R0 ← old R1
-    MOV  R1, R2  // R1 ← new R2
-
-    // decrement & maybe finish
-    SUB  R5, R5, R6
-    BNE LOOP
+STR R0, R8              # store current F[n-1] to memory at address R8
+ADD R2, R0, R1          # compute next = R0 + R1
+ADD R4, R4, R6          # increment address pointer
+MOV R0, R1              # R0 ← old R1
+MOV R1, R2              # R1 ← new R2
+SUB R5, R5, R6          # decrement loop counter
+BNE LOOP                # branch if not zero
 HALT
--- Compiler/testing.asm
+++ Compiler/testing.asm
@@ -1,16 +1,16 @@
 // Functional opcodes: ADD, SUB, XOR, LDI (load data immediate), JMP (jump/goto), BEQZ (branch if equals zero), HALT, STR (store), AND, LOAD, MOV (move), JAL, JR, MUL
 
 
-LDI R0, #0   // Load value into R0
-LDI R1, #10    // Load value into R1
+LDI R0, 0   // Load value into R0
+LDI R1, 10    // Load value into R1
 ADD R1, R1, R1
 JAL R15, FUNC
-LDI R5, #1
+LDI R5, 1
 MOV R1, R2 // Move value from R2 to R1
 HALT
 
 FUNC:
-LDI R3, #16
+LDI R3, 16
 MUL R2, R3, R1
 
 JR R15
